e909c4f0520f9980103ce9de5066050439d0bbd7
Pakhapong Kanchanamai <Pakhapong.Kanchanamai@nikon.com>
2026-01-14 13:24:02 +0700

    Add dxDataGrid

---
+++ iLearn.Admin/Controllers/AssignmentRulesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace iLearn.Admin.Controllers
+{
+    public class AssignmentRulesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ iLearn.Admin/Controllers/LearningLogsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace iLearn.Admin.Controllers
+{
+    public class LearningLogsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
