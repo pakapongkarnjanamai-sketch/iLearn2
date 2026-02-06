db7ff590dd8e5e42b34a847c0385253f400964b2
Pakhapong Kanchanamai <Pakhapong.Kanchanamai@nikon.com>
2026-02-06 12:59:50 +0700

    Add User

---
+++ iLearn.User/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace iLearn.User.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ iLearn.User/Controllers/MyLearningController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iLearn.User.Controllers
+{
+    [Authorize]
+    public class MyLearningController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public MyLearningController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Player(int enrollmentId)
+        {
+            ViewBag.EnrollmentId = enrollmentId;
+          
+            return View();
+        }
+    }
+}
